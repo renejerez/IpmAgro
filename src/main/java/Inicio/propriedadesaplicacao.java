72585ab02be454ed113b60f6a5596c79f780210c
renejerez <rene_jerez@hotmail.com>
2018-02-09 00:11:47 -0200

    identificação dos demais campos e teste com um caso fixo, hardcode.

--- src/main/java/Inicio/propriedadesaplicacao.java
+++ src/main/java/Inicio/propriedadesaplicacao.java
@@ -41,24 +41,58 @@ public class propriedadesaplicacao {
 				/*capacidade.setCapability("appPackage", "com.whatsapp");
 				capacidade.setCapability("appActivity", "com.whatsapp.Main");*/
 				
-		/*WebDriverWait wait = new WebDriverWait(AppiumController.instance.driver,timeout);*/
+		/*Aguardar a tela carregar para clicar*/
 		Thread.sleep(20000);
 				
+		/*Clicar no botão iniciar do IPMAgro*/
 		driver.findElement(By.xpath("//android.widget.ImageView[@bounds = '[213,858][327,960]']")).click();
+		Thread.sleep(5000);
 		
-				
-		/*driver.findElement(By.className("android.view.ViewGroup")).click();*/ 
+		/*Preencher o campo de Categoria*/
+		driver.findElement(By.xpath("//android.widget.EditText[@bounds = '[24,169][516,237]']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//android.widget.TextView[@text = 'TRATOR']")).click();
+		Thread.sleep(5000);
+		
+		/*Preencher o campo de Marca*/
+		driver.findElement(By.xpath("//android.widget.EditText[@bounds = '[24,277][516,345]']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//android.widget.TextView[@text = 'AGRALE']")).click();
+		Thread.sleep(5000);
+		
+		/*Preencher o campo de Modelo*/
+		driver.findElement(By.xpath("//android.widget.EditText[@bounds = '[24,385][516,453]']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//android.widget.TextView[@text = '4100 4X2']")).click();
+		Thread.sleep(5000);
+		
+		/*Preencher o campo de Ano*/
+		driver.findElement(By.xpath("//android.widget.EditText[@bounds = '[24,493][516,561]']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//android.widget.TextView[@text = '2016']")).click();
+		Thread.sleep(5000);
+		
+		/*Preencher o campo de UF*/
+		driver.findElement(By.xpath("//android.widget.EditText[@bounds = '[24,601][516,669]']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//android.widget.TextView[@text = 'ACRE']")).click();
+		Thread.sleep(5000);
 		
 		
-		/*driver.findElement(By.id("")).sendKeys("3");
+		/*Clicar no botão iniciar de buscar*/
+		driver.findElement(By.xpath("//android.widget.Button[@bounds = '[24,684][516,756]']")).click();
+		Thread.sleep(10000);
+		
+		String ValorEquipamento = driver.findElement(By.xpath("//android.widget.TextView[@bounds = '[18,677][266,721]']")).getText();
+		
+		System.out.println(ValorEquipamento);
+		
+		/*-driver.findElement(By.id("")).sendKeys("3");
 		driver.findElement(By.id("br.com.libertyseguros.mobile.corretor:id/btnLogin")).click();
 		driver.findElement(By.id("br.com.libertyseguros.mobile.corretor:id/editUsuarioLogin")).sendKeys("renevj");
 		driver.findElement(By.id("br.com.libertyseguros.mobile.corretor:id/editSenhaLogin")).sendKeys("Li1234!@");
 		driver.findElement(By.id("br.com.libertyseguros.mobile.corretor:id/btnLogin")).click();
 		Assert.assertEquals("O triângulo é Equilátero", driver.findElement(By.id("com.eliasnogueira.trianguloapp:id/txtResultado")).getText());*/
-		
-		
-		System.out.println("Terminou a execução com sucesso !");
 		
 	}
 	
